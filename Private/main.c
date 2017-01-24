0720b8829d25f292920ce8927ee74e17a6c9fcff
Martin Bobek <martinbobek77@hotmail.com>
2017-01-24 00:02:34 -0700

    Setup of crystal clock and pll.

--- Private/main.c
+++ Private/main.c
@@ -22,7 +22,57 @@ volatile uint32_t G_u32SystemTick = 0u;
 
 int main()
 {
-  __disable_irq();
+    //__disable_irq();
+  
+  /* Clock Initialization */
+  
+  /*
+  Steps:
+    -Set the MOSCXTEN bit, MOSCSEL bit and write startup time in MOSCXTST field in CKGR_MOR.
+    -poll MOSCXTS bit in PMC_SR until it is on. Then move on.
+    -poll MOSCSELS bit in PMC_SR to see when this change is done. Then move on.
+    -write 1 to DIVA, 7 to MULA, an appropriate value to PLLACOUNT, and 1 to bit 29 in CKGR_PLLAR.
+    -poll LOCKA bit in PMC_SR untill PLLA is running. Then move on.
+    -Write 1 to FWS in EEFC_FMR for EEFC0
+    -Write 1 to Pres in PMC_MCKR
+    -poll MCKRDY bit in PMC_SR. then move on.
+    -Write 2 to CSS in PMC_MCKR.
+    -poll MCKRDY bin in PMC_SR. then move on.
+    -clear MOSCRCEN bit in CKGR_MOR (be carefull not to overwrite previous state of CKGR_MOR) (MOSCRCS in PMC_SR should automatically be cleared indicating clock is off) (can this be safely done in first write)
+    -write 4 to CSS and 1 to PRES in PMC_PCK[0]
+    -write 1 to PCK0 bit in PMC_SCER
+    
+  Activate USB UTMI PLL and wait for stabalization.
+    turn on and enable crystal oscillator
+    
+  
+  Need to configure PIOA to enable peripheral B output on PA27
+  Check LPM bit in PMC_FSMR (could prevent the processor from sleeping)
+  Select between RC and Crystal slow lock: XTALSEL bit in SUPC_CR - default is RC oscillator 
+  */
+  
+  PMC->CKGR_MOR = CKGR_MOR_MOSCXTEN | CKGR_MOR_MOSCRCEN | CKGR_MOR_MOSCXTST(240) | CKGR_MOR_KEY_PASSWD; // where does this value come from
+  while (!(PMC->PMC_SR & PMC_SR_MOSCXTS));
+  PMC->CKGR_MOR = CKGR_MOR_MOSCXTEN | CKGR_MOR_MOSCSEL | CKGR_MOR_KEY_PASSWD;
+  while (!(PMC->PMC_SR & PMC_SR_MOSCSELS));
+  PMC->CKGR_PLLAR = CKGR_PLLAR_DIVA(1) | CKGR_PLLAR_MULA(7) | CKGR_PLLAR_PLLACOUNT(63) | CKGR_PLLAR_ONE; // where does this value come from
+  while (!(PMC->PMC_SR & PMC_SR_LOCKA));
+  EFC0->EEFC_FMR = EEFC_FMR_FWS(2);
+  PMC->PMC_MCKR = PMC_MCKR_CSS_MAIN_CLK | PMC_MCKR_PRES_CLK_2;
+  while (!(PMC->PMC_SR & PMC_SR_MCKRDY));
+  PMC->PMC_MCKR = PMC_MCKR_CSS_PLLA_CLK | PMC_MCKR_PRES_CLK_2;
+  while (!(PMC->PMC_SR & PMC_SR_MCKRDY));
+  PMC->PMC_PCK[0] = PMC_PCK_CSS_MCK;
+  PMC->PMC_SCER = PMC_SCER_PCK0;
+  
+  
+  
+  
+  //SUPC->SUPC_CR = SUPC_CR_XTALSEL | SUPC_CR_KEY_PASSWD;             /* Selects 32.768kHz (external) crystal oscillator (instead of RC) for Slow Clock */
+  
+  
+  
+  /* End Clock Initialization */
   
   PMC->PMC_PCER0 = ID_Msk(ID_PIOA) | ID_Msk(ID_PIOB);               /* Peripheral clock enabled for PIO A and B */
   PIOA->PIO_PER = BUTTON_ALL_A | HEARTBEAT;                         /* Enables PIO A control of selected pins */
@@ -33,22 +83,17 @@ int main()
   PIOB->PIO_PUDR = LED_ALL | BUTTON_ALL_B;          // | LCD_ALL    /* Disables PIO B pull-ups for select pins */
   PIOA->PIO_AIMER = BUTTON_ALL_A;                                   /* Enables additional PIO A detection modes for buttons */
   PIOB->PIO_AIMER = BUTTON_ALL_B;                                   /* Enables additional PIO B detection modes for buttons */
-  PIOA->PIO_IER = BUTTON_ALL_A;                                     /* Enables PIO A button interupts */
-  PIOB->PIO_IER = BUTTON_ALL_B;                                     /* Enables PIO B button interupts */
   PIOA->PIO_WPMR = PIO_WPMR_WPEN | PIO_WPMR_WPKEY(0x50494F);        /* Enables PIO A write protection */
   PIOB->PIO_WPMR = PIO_WPMR_WPEN | PIO_WPMR_WPKEY(0x50494F);        /* Enables PIO B write protection */
-  NVIC->ISER[0] = ID_Msk(PIOA_IRQn) | ID_Msk(PIOB_IRQn);            /* Enables NVIC interrupt handling for PIO A and B interrupts */
-  NVIC->IP[PIOA_IRQn] = IP_Msk(2);                                  /* Sets PIO A interrupt priority to 2 */
-  NVIC->IP[PIOB_IRQn] = IP_Msk(2);                                  /* Sets PIO B interrupt priority to 2 */
+  
+  // Add PMC write protection (WPEN bit)
   
   User_Initialization();
   
   SysTick->LOAD = MCK_HZ / 1000 - 1;                                /* Sets SysTick to 1ms */
   SCB->SHP[11] = IP_Msk(1);                                         /* Sets SysTick exception priority to 1 */
-  PIOA->PIO_ISR;
-  PIOB->PIO_ISR;
-  NVIC->ICPR[0] = 0xFFFFFFFF;                                       /* Clear all pending interrupts */
-  __enable_irq();                                                   /* Enables inerrupts */          
+  //NVIC->ICPR[0] = 0xFFFFFFFF;                                       /* Clear all pending interrupts */
+  __enable_irq();                                                   /* Enables interrupts */          
   SysTick->CTRL = SysTick_CTRL_CLKSOURCE_Msk |                      /* Sets SysTick clock source as MCK, ... */
     SysTick_CTRL_ENABLE_Msk | SysTick_CTRL_TICKINT_Msk;             /* ... enables Systick exception request, and activates the timer */
   
